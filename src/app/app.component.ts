0a3db285bd0b294733f924803d2e4519f0cfa960
Mrun9 <mrunalvibhute963@gmail.com>
2023-04-09 13:39:04 +0530

    edit implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'front-end';
+  employees: any[] = [];
+  isEdit = false;
+
+  employee={
+    id: 0,
+    name: ''
+  }
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  getEmployees() {
+    this.http.get('http://localhost:3000').subscribe(
+      {
+        next: (res: any) => {
+          this.employees = res;
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      }
+    );
+  }
+
+  addEmployee() {
+    this.http.post('http://localhost:3000', this.employee).subscribe(
+      {
+        next: (res: any) => {
+          this.getEmployees();
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      }
+    );
+  }
+
+  deleteEmployee(id: any) {
+    this.http.delete('http://localhost:3000/' + id).subscribe(
+      {
+        next: (res: any) => {
+          this.getEmployees();
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      }
+    );
+  }
+
+  onEditClick(employee: any) {
+    this.isEdit = true;
+    this.employee = { id: employee.id, name: employee.name};
+  }
+
+  cancelEdit() {
+    this.isEdit = false;
+    this.employee = {id: 0, name: ''};
+  }
+
+  saveEmployee() {
+    this.http.put('http://localhost:3000', this.employee).subscribe(
+      {
+        next: (res: any) => {
+          this.cancelEdit();
+          this.getEmployees();
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      }
+    );
+  }
+
 }
